517843e7b58e78d028eb5bd3e9f52ca473d2b7ea
Elliott Pryor <pryorelliott@gmail.com>
2021-10-19 16:37:01 -0600

    Command line args

--- HW3/suffix_array.py
+++ HW3/suffix_array.py
@@ -1,7 +1,9 @@
+import sys
+
 
 def suffix_array(S):
     suffix = [(i, S[i:]) for i in range(len(S))]  # generate list of tuples
-    suffix.sort(key=lambda tup: tup[1])  # sort by suffix's
+    suffix.sort(key=lambda tup: tup[1])  # sort by suffixes
     return [fix[0] for fix in suffix]
 
 
@@ -13,7 +15,7 @@ def pattern_search(pattern, S):
         m = int((l + r) / 2)
         st = S[suffix[m]:]  # string version of the suffix
         if pattern == st[:len(pattern)]:
-            print(f'Found match at {suffix[m]}')
+            print(f'Found a match at {suffix[m]}')
             found = True
             break
         elif pattern > st:
@@ -24,6 +26,22 @@ def pattern_search(pattern, S):
     if not found:
         print("Pattern not found in string")
 
+
 if __name__ == '__main__':
-    print(suffix_array("banana$"))
-    pattern_search('ba', 'banana$')
+    pattern = None
+
+    if len(sys.argv) == 2:
+        input_str = sys.argv[1]
+    elif len(sys.argv) == 3:
+        input_str = sys.argv[1]
+        pattern = sys.argv[2]
+    else:
+        input_str = input("Input the string to compute a suffix array for: ")
+
+    if pattern is None:
+        pattern = input("Input the pattern to search for: ")
+
+    print(f"Computing suffix array for: {input_str}")
+    print(suffix_array(input_str))
+    print(f"Finding the pattern: {pattern}")
+    pattern_search(pattern, input_str)
