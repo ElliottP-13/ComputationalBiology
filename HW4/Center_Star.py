1b7ae167c804ad8d388349fec41a99a6d94e0f67
ElliottP-13 <pryorelliott@gmail.com>
2021-11-09 11:07:55 -0700

    Compute MSA

--- HW4/Center_Star.py
+++ HW4/Center_Star.py
@@ -2,6 +2,16 @@ from EditDistance import *
 import numpy as np
 
 
+def compute_indx(Sc,s2):
+    indx = []
+    for i in range(len(s2)):
+        if i >= len(Sc) or (s2[i] == '_' and Sc[i] != '_'):
+            indx.append(i)
+            Sc = Sc[:i] + '_' + Sc[i:]
+    indx += [len(Sc) + i for i in range(len(s2) - len(Sc))]
+    return indx
+
+
 def center_star(strings):
     """
     :param strings: List of strings to compute
@@ -22,6 +32,29 @@ def center_star(strings):
     return strings[center], center
 
 
+def MSA(strings):
+    Sc, center = center_star(strings)
+    alignment = [s for s in strings]  # copy the input strings
+    for i in range(len(strings)):
+        if i == center:  # skip center string
+            continue
+
+        V, P = needleman(Sc, strings[i])
+        s2, t2 = construct_alignment(P, Sc, strings[i])
+        alignment[i] = t2
+
+        add_spaces = compute_indx(Sc, s2)
+        for indx in add_spaces:
+            for j in range(i):
+                alignment[j] = alignment[j][:indx] + '_' + alignment[j][indx:]
+        Sc = s2
+    alignment[center] = Sc
+
+    return alignment
+
+
 if __name__ == "__main__":
     print(center_star(['bananan', 'bann', 'aannnana', 'ananana', 'bananan']))
-    pass
+    msa = MSA(['bananan', 'bann', 'aannnana', 'ananana', 'bananan'])
+    for s in msa:
+        print(s)
