6e30cb8716072b5866d62ad6224511eda8f68d83
Elliott Pryor <pryorelliott@gmail.com>
2021-11-12 15:50:28 -0700

    HW 4

--- HW4/Center_Star.py
+++ HW4/Center_Star.py
@@ -12,7 +12,7 @@ def compute_indx(Sc,s2):
     return indx
 
 
-def center_star(strings):
+def center_star(strings, f=delta):
     """
     :param strings: List of strings to compute
     :return: center string, index of center string in strings array (param)
@@ -32,14 +32,14 @@ def center_star(strings):
     return strings[center], center
 
 
-def MSA(strings):
+def MSA(strings, f=delta):
     Sc, center = center_star(strings)
     alignment = [s for s in strings]  # copy the input strings
     for i in range(len(strings)):
         if i == center:  # skip center string
             continue
 
-        V, P = needleman(Sc, strings[i])
+        V, P = needleman(Sc, strings[i], f)
         s2, t2 = construct_alignment(P, Sc, strings[i])
         alignment[i] = t2
 
@@ -53,8 +53,60 @@ def MSA(strings):
     return alignment
 
 
+def read_fasta(filepath):
+    """
+    Returns a dict key=sequence_name, value=sequence from the fasta file
+    :param filepath:
+    :return:
+    """
+    file = open(filepath)
+    print(f"Opening: {filepath}")
+    started = False
+    header = ''
+    text = ''
+    sequences = {}
+    for line in file:  # reads in fasta file. Allows for sequence to continue on multiple lines
+        if line.startswith('>'):
+            if started:
+                sequences[header] = text.replace('\n', '')
+                text = ''
+            started = True
+            header = line.replace('>', '').strip()
+        else:
+            text += line.strip()
+    sequences[header] = text.replace('\n', '')
+    return sequences
+
+
+def diff_generator(a, b):
+    def f(c1, c2):
+        # Helper function used for scoring
+        if c1 == c2:
+            return 0
+        elif c1 == '_' or c2 == '_':
+            return b
+        else:
+            return a
+    return f
+
+
 if __name__ == "__main__":
-    print(center_star(['bananan', 'bann', 'aannnana', 'ananana', 'bananan']))
-    msa = MSA(['bananan', 'bann', 'aannnana', 'ananana', 'bananan'])
-    for s in msa:
-        print(s)
+    if len(sys.argv) == 4:
+        alpha = float(sys.argv[1])
+        beta = float(sys.argv[2])
+        file = sys.argv[3]
+        seq = read_fasta(file)
+        f = diff_generator(alpha, beta)
+        ins = list(seq.values())
+
+        print(f'Center (string, index): {center_star(ins, f)}')
+        print("MSA:")
+        msa = MSA(ins, f)
+        for s in msa:
+            print(s)
+    else:
+        print(center_star(['ACTCTCGATC', 'ACTTCGATC', 'ACTCTCTATC', 'ACTCTCTAATC']))
+        print(center_star(['bananan', 'bann', 'aannnana', 'ananana', 'bananan']))
+        msa = MSA(['bananan', 'bann', 'aannnana', 'ananana', 'bananan'])
+        for s in msa:
+            print(s)
