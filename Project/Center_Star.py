63b9b202b9ebee8b0bee53e64b83c16bdf0bb6dc
Elliott Pryor <pryorelliott@gmail.com>
2021-12-03 11:56:05 -0700

    Refactor center star into class

--- Project/Center_Star.py
+++ Project/Center_Star.py
@@ -1,56 +1,65 @@
 from EditDistance import *
 import numpy as np
 
+class CenterStar:
+    def __init__(self, f=delta):
+        self.f = f
 
-def compute_indx(Sc,s2):
-    indx = []
-    for i in range(len(s2)):
-        if i >= len(Sc) or (s2[i] == '_' and Sc[i] != '_'):
-            indx.append(i)
-            Sc = Sc[:i] + '_' + Sc[i:]
-    indx += [len(Sc) + i for i in range(len(s2) - len(Sc))]
-    return indx
+    @staticmethod
+    def compute_indx(Sc, s2):
+        """
+        Compute where to add blanks when comparing 2 strings
+        :param Sc:
+        :param s2:
+        :return:
+        """
+        indx = []
+        for i in range(len(s2)):
+            if i >= len(Sc) or (s2[i] == '_' and Sc[i] != '_'):
+                indx.append(i)
+                Sc = Sc[:i] + '_' + Sc[i:]
+        indx += [len(Sc) + i for i in range(len(s2) - len(Sc))]
+        return indx
+
+    def center_star(self, strings):
+        """
+        :param strings: List of strings to compute
+        :return: center string, index of center string in strings array (param)
+        """
+
+        mat = np.zeros((len(strings), len(strings)))  # make k x k matrix
+        for i, S in enumerate(strings):
+            for j, T in enumerate(strings):
+                if j <= i:  # skip the lower diagonal elements
+                    continue
+                D = needleman(S, T, self.f)[0]
+                mat[i][j] = D[len(S)][len(T)]  # get the edit distance
+                mat[j][i] = mat[i][j]
+
+        # the string that minimizes sum of distances to all other strings
+        center = np.argmin(mat.sum(axis=1))
+        return strings[center], center
 
 
-def center_star(strings, f=delta):
-    """
-    :param strings: List of strings to compute
-    :return: center string, index of center string in strings array (param)
-    """
-
-    mat = np.zeros((len(strings), len(strings)))  # make k x k matrix
-    for i, S in enumerate(strings):
-        for j, T in enumerate(strings):
-            if j <= i:  # skip the lower diagonal elements
+    def MSA(self, strings):
+        Sc, center = self.center_star(strings)
+        alignment = [s for s in strings]  # copy the input strings
+        for i in range(len(strings)):
+            if i == center:  # skip center string
                 continue
-            D = needleman(S, T, f)[0]
-            mat[i][j] = D[len(S)][len(T)]  # get the edit distance
-            mat[j][i] = mat[i][j]
 
-    # the string that minimizes sum of distances to all other strings
-    center = np.argmin(mat.sum(axis=1))
-    return strings[center], center
+            V, P = needleman(Sc, strings[i], self.f)
+            s2, t2 = construct_alignment(P, Sc, strings[i])
+            alignment[i] = t2
 
+            add_spaces = self.compute_indx(Sc, s2)  # compute index where to add spaces
+            for indx in add_spaces:
+                for j in range(i):
+                    alignment[j] = alignment[j][:indx] + '_' + alignment[j][indx:]
+            Sc = s2
+        alignment[center] = Sc
 
-def MSA(strings, f=delta):
-    Sc, center = center_star(strings)
-    alignment = [s for s in strings]  # copy the input strings
-    for i in range(len(strings)):
-        if i == center:  # skip center string
-            continue
-
-        V, P = needleman(Sc, strings[i], f)
-        s2, t2 = construct_alignment(P, Sc, strings[i])
-        alignment[i] = t2
-
-        add_spaces = compute_indx(Sc, s2)
-        for indx in add_spaces:
-            for j in range(i):
-                alignment[j] = alignment[j][:indx] + '_' + alignment[j][indx:]
-        Sc = s2
-    alignment[center] = Sc
-
-    return alignment
+        return alignment
 
 
 def read_fasta(filepath):
@@ -99,14 +108,17 @@ if __name__ == "__main__":
         f = diff_generator(alpha, beta)
         ins = list(seq.values())
 
-        print(f'Center (string, index): {center_star(ins, f)}')
+        c = CenterStar(f)
+
+        print(f'Center (string, index): {c.center_star(ins)}')
         print("MSA:")
-        msa = MSA(ins, f)
+        msa = c.MSA(ins)
         for s in msa:
             print(s)
     else:
-        print(center_star(['ACTCTCGATC', 'ACTTCGATC', 'ACTCTCTATC', 'ACTCTCTAATC']))
-        print(center_star(['bananan', 'bann', 'aannnana', 'ananana', 'bananan']))
-        msa = MSA(['bananan', 'bann', 'aannnana', 'ananana', 'bananan'])
+        c = CenterStar()
+        print(c.center_star(['ACTCTCGATC', 'ACTTCGATC', 'ACTCTCTATC', 'ACTCTCTAATC']))
+        print(c.center_star(['bananan', 'bann', 'aannnana', 'ananana', 'bananan']))
+        msa = c.MSA(['bananan', 'bann', 'aannnana', 'ananana', 'bananan'])
         for s in msa:
             print(s)
